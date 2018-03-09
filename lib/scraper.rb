f7e5c3ddfc4bf4b8e482d64ff10e4bbd77bfa286
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-09 01:39:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,8 @@ class Scraper
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     students.each do |student|
-      name = student.css(".student-name").text 
+      name = student.css(".student-name").text
+    end 
   end
 
   def self.scrape_profile_page(profile_url)
