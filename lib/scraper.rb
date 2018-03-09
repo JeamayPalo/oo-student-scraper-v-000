2d45f220334a13cb7426e7e4c2fefa0a0d42bdf3
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-09 01:39:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_list = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
