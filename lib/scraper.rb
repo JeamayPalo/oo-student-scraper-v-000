bfe16f79f7746436e3ab2da82144ee5f62d64de5
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-09 01:56:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
     end
     names_array
 
-    # This block returns a list of locations.
+  # Student Locations 
     locations = list.css(".student-location")
     location_array = []
     locations.each do |item|
