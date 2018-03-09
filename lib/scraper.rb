c39e7ef83c7622260b76827e5f0d4f990b991bb4
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-09 01:59:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,21 @@
-require 'open-uri'
 require 'pry'
-require 'nokogiri' #=> I added this one
-require 'rubygems' #=> I added this one
+require 'nokogiri'
+
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    html = open(index_url) #=> index_url = "../fixtures/student-site/index.html"
+   def self.scrape_index_page(index_url)
+    html = open(index_url)
     list = Nokogiri::HTML(html)
 
-    # This block returns a list of student names
-    names = list.css(".student-name")
+  # Student Names
+    student_names = list.css(".student-name")
     names_array = []
-    names.each do |item|
+    student_names.each do |item|
       names_array << item.text
     end
     names_array
 
-    # This block returns a list of locations.
+  # Student Locations 
     locations = list.css(".student-location")
     location_array = []
     locations.each do |item|
@@ -24,14 +23,13 @@ class Scraper
     end
     location_array
 
-    # This block returns a list of student HTML pages.
+    # HTML Pages
     webpages = list.css(".student-card a[href]")
     webpage_array = []
     webpages.select do |item|
       webpage_array << item['href']
     end
     webpage_array
-
 
     master_array = []
     hash = {}
@@ -41,65 +39,5 @@ class Scraper
       x = x + 1
     end
     master_array
-  end
-
-  def self.scrape_profile_page(profile_url)
-    html = open(profile_url)
-    doc = Nokogiri::HTML(html)
-
-    students_twitter = []
-    students_linkedin = []
-    students_github = []
-    students_youtube = []
-    students_blog = []
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("twitter")
-        students_twitter << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("linkedin")
-        students_linkedin << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("github")
-        students_github << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if !(link['href'].include?("github")) &&
-         !(link['href'].include?("linkedin")) &&
-         !(link['href'].include?("twitter"))
-         students_blog = link['href']
-      end
-    end
-
-    students_quote = doc.css(".profile-quote").text
-    students_bio = doc.css(".description-holder p").text
-
-    students_twitter
-    students_linkedin
-    students_github
-    students_youtube
-      if students_youtube = ""
-        students_youtube = nil
-      end
-    students_blog
-
-    # This needs to be an iteration where it starts with <<output = {}>>
-    out_put = {:twitter=>students_twitter[0],
-               :linkedin=>students_linkedin[0],
-               :github=>students_github[0],
-               :profile_quote=>students_quote,
-               :blog=>students_blog,
-               :bio=> students_bio}
-    out_put.delete_if {|key, value| value == [] || value == nil}
-
-    out_put
   end
 end
